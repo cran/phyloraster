5dfaf0404ce4f7d10e8ff737c850295e671fa9e8
Gabriela Alves-Ferreira <gabriela-alves77@hotmail.com>
2025-02-28 15:00:01 +0000

    version 2.2.0

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 <!-- badges: start -->
-[![CRAN-status](https://www.r-pkg.org/badges/version/phyloraster)](https://cran.r-project.org/package=phyloraster)
-[![R-CMD-check](https://github.com/gabferreira/phyloraster/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/gabferreira/phyloraster/actions/workflows/R-CMD-check.yaml)
-[![codecov](https://codecov.io/gh/gabferreira/phyloraster/branch/master/graph/badge.svg)](https://app.codecov.io/gh/gabferreira/phyloraster)
-[![download](http://cranlogs.r-pkg.org/badges/grand-total/phyloraster?color=green)](https://cran.r-project.org/package=phyloraster)
-[![download per month](http://cranlogs.r-pkg.org/badges/phyloraster?color=green)](https://cran.r-project.org/package=phyloraster)
+
+[![CRAN-status](https://www.r-pkg.org/badges/version/phyloraster)](https://cran.r-project.org/package=phyloraster) [![R-CMD-check](https://github.com/gabferreira/phyloraster/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/gabferreira/phyloraster/actions/workflows/R-CMD-check.yaml) [![codecov](https://codecov.io/gh/gabferreira/phyloraster/branch/master/graph/badge.svg)](https://app.codecov.io/gh/gabferreira/phyloraster) [![download](https://cranlogs.r-pkg.org/badges/grand-total/phyloraster?color=green)](https://cran.r-project.org/package=phyloraster) [![download per month](https://cranlogs.r-pkg.org/badges/phyloraster?color=green)](https://cran.r-project.org/package=phyloraster)
+
 <!-- badges: end -->
 
 # phyloraster <a href="https://gabferreira.github.io/phyloraster/"><img src="man/figures/logo.png" alt="phyloraster website" align="right" height="139"/></a>
@@ -17,32 +15,36 @@
 ### Installation
 
 The CRAN version of the package can be installed using:
-```
+
+```         
 install.packages("phyloraster") 
 ```
 
 The development version of `phyloraster` can be downloaded from [GitHub](https://github.com/gabferreira/phyloraster) using the following code:
-```
+
+```         
 devtools::install_github("gabferreira/phyloraster")
 ```
 
 <br>
 
-### phyloraster basics
+### Example usage
 
-Basic information about the package can be found at the [package's webpage](https://gabferreira.github.io/phyloraster/) or in the vignette `vignette("phyloraster-vignette")`
+Example usage and information about the package can be found at the [package's webpage](https://gabferreira.github.io/phyloraster/) or in the vignette `vignette("phyloraster-vignette")`
 
 <br>
 
 ### Citation
 
-- Thank you for using phyloraster! Please remember to cite phyloraster in your publications: <br>
+Please cite both the package and the article describing the methods in your publications:. Here is an example:
 
-Alves-Ferreira G., Heming N. M., Mota F. M. M. (2023). phyloraster: Evolutionary Diversity Metrics for Raster Data. R package version 2.0.
+-  Article recently published in [`Ecography`](https://doi.org/10.1111/ecog.06902):
+Alves-Ferreira G, Mota FMM, Talora DC, Oliveira, C, Solé, MK, Heming NM (2024). phyloraster: an R package to calculate measures of endemism and evolutionary diversity for rasters. Ecography. doi: 10.1111/ecog.06902.
 
-Alves-Ferreira G, Mota FMM, Talora DC, Oliveira, C, Solé, MK, Heming NM (2023). phyloraster: an R package to calculate measures of endemism and evolutionary diversity for rasters. Ecography. doi: 10.1111/ecog.06902. 
+-  Package published in [`CRAN`](https://CRAN.R-project.org/package=phyloraster):
+Alves-Ferreira G, Heming NM, Mota FMM. (2024). phyloraster: Evolutionary Diversity Metrics for Raster Data. R package version 2.0. doi: 10.32614/CRAN.package.phyloraster
 
-- For more information:
+For more information:
 
 ```         
 citation("phyloraster")
@@ -52,6 +54,6 @@ citation("phyloraster")
 
 ### Issues
 
-If you have any question or find any bug, let us know through the topic ["Issues"](https://github.com/gabferreira/phyloraster/issues).
+If you have any question or find any bug, let us know through the topic [Issues](https://github.com/gabferreira/phyloraster/issues).
 
 <br>
--- inst/doc/phyloraster-vignette.R
+++ inst/doc/phyloraster-vignette.R
@@ -4,7 +4,7 @@ knitr::opts_chunk$set(collapse = TRUE,message=FALSE, warning = FALSE,
                       comment = "#>")
 
 ## ----eval = FALSE-------------------------------------------------------------
-#   install.packages("phyloraster")
+#  install.packages("phyloraster")
 
 ## ----eval = FALSE-------------------------------------------------------------
 #  devtools::install_github("gabferreira/phyloraster")
@@ -18,7 +18,10 @@ library(phylobase)
 
 ## -----------------------------------------------------------------------------
 data <- load.data.rosauer()
-head(data$presab)
+head(data$presab[,1:7])
+
+## ----fig.height = 5, fig.width = 5, fig.align = 'center'----------------------
+plot(data$raster[[1]], cex = 0.65)
 
 ## -----------------------------------------------------------------------------
 data$tree
@@ -60,7 +63,7 @@ plot(r2[[9]])
 library(terra)
 
 shp <- terra::vect(system.file("extdata", "shps_iucn_spps_rosauer.shp",
-                              package="phyloraster"))
+                               package="phyloraster"))
 
 # create a polygon to use as mask with an extent
 e <- terra::ext(113, 123, -43.64, -33.90)
@@ -69,11 +72,11 @@ p <- terra::as.polygons(e, crs="")
 coun.crop <- terra::crop(p, 
                          terra::ext(shp)) 
 coun.rast <- terra::rasterize(coun.crop,
-terra::rast(terra::ext(shp), resolution = 0.5))
+                              terra::rast(terra::ext(shp), resolution = 0.5))
 
 # rasterizing with the mask of the polygon
 shp.t <- shp2rast(shp, y = coun.rast, sps.col = "BINOMIAL", ymask = TRUE)
-plot(shp.t[[1]], col = c("grey", "green"))
+plot(shp.t[[1]])
 
 ## -----------------------------------------------------------------------------
 data <- load.data.rosauer()
@@ -88,6 +91,18 @@ dataprep <- phylo.pres(x = ras, tree = tree)
 
 ## -----------------------------------------------------------------------------
 names(dataprep$x) == tree$tip.label
+
+## -----------------------------------------------------------------------------
+ras <- terra::rast(system.file("extdata", "rast.presab.tif", 
+                               package = "phyloraster"))
+tree <- ape::read.tree(system.file("extdata", "tree.nex", 
+                                   package = "phyloraster"))
+
+# Using the full tree
+dataprep_full <- phylo.pres(x = ras, tree = tree, full_tree_metr = TRUE)
+
+# Using the prunned tree
+dataprep_sub <- phylo.pres(x = ras, tree = tree, full_tree_metr = FALSE) 
 
 ## ----fig.height = 5, fig.width = 4, fig.align = 'center', warning= FALSE, echo = FALSE----
 knitr::include_graphics("figs/tree.jpg")
@@ -105,7 +120,6 @@ plot(sr, main = "Species richness")
 ras <- terra::rast(system.file("extdata", "rast.presab.tif", 
                                package = "phyloraster"))
 wer <- rast.we(x = ras)
-wer
 
 ## ----fig.height = 5, fig.width = 7, fig.align = 'center', warning= FALSE------
 wer$WE
@@ -116,10 +130,9 @@ ras <- terra::rast(system.file("extdata", "rast.presab.tif",
                                package = "phyloraster"))
 tree <- ape::read.tree(system.file("extdata", "tree.nex", 
                                    package = "phyloraster"))
-dataprep <- phylo.pres(x = ras, tree = tree, pruning = "tree")
+dataprep <- phylo.pres(x = ras, tree = tree, full_tree_metr = TRUE)
 
-pdr <- rast.pd(x = dataprep$x, edge.path = dataprep$edge.path, 
-               branch.length = dataprep$branch.length)
+pdr <- rast.pd(x = dataprep$x, dataprep$tree)
 
 ## ----pdr-plot, fig.height = 5, fig.width = 7, fig.align = 'center', warning= FALSE----
 plot(pdr$PD, main = "Phylogenetic diversity")
@@ -129,43 +142,34 @@ ras <- terra::rast(system.file("extdata", "rast.presab.tif",
                                package = "phyloraster"))
 tree <- ape::read.tree(system.file("extdata", "tree.nex", 
                                    package = "phyloraster"))
-per <- rast.pe(x = dataprep$x, tree)
+dataprep <- phylo.pres(x = ras, tree = tree, full_tree_metr = TRUE)
+
+per <- rast.pe(x = dataprep$x, dataprep$tree)
 per
 
 ## ----per-plot, fig.height = 5, fig.width = 7, fig.align = 'center', warning= FALSE----
 plot(per$PE, main = "Phylogenetic Endemism")
 
 ## ----warning= FALSE-----------------------------------------------------------
-x <- terra::rast(system.file("extdata", "rast.presab.tif", package="phyloraster"))
+x <- terra::rast(system.file("extdata", "rast.presab.tif", 
+                             package="phyloraster"))
 # phylogenetic tree
-tree <- ape::read.tree(system.file("extdata", "tree.nex", package="phyloraster"))
-data <- phylo.pres(x, tree)
-ed <- rast.ed(data$x, tree)
+tree <- ape::read.tree(system.file("extdata", "tree.nex", 
+                                   package="phyloraster"))
+dataprep <- phylo.pres(x = ras, tree = tree, full_tree_metr = TRUE)
+
+ed <- rast.ed(dataprep$x, dataprep$tree)
 ed
 
 ## ----edr-plot, fig.height = 5, fig.width = 7, fig.align = 'center', warning= TRUE----
 terra::plot(ed, main = "Evolutionary Distinctiveness")
 
 ## ----warning= FALSE-----------------------------------------------------------
-tree <- ape::read.tree(system.file("extdata", "tree.nex", package="phyloraster"))
+tree <- ape::read.tree(system.file("extdata", "tree.nex", 
+                                   package="phyloraster"))
 
 ed <- phyloraster::species.ed(tree)
 head(ed)
-
-## -----------------------------------------------------------------------------
-library(SESraster)
-ras <- terra::rast(system.file("extdata", "rast.presab.tif", 
-                               package = "phyloraster"))
-tree <- ape::read.tree(system.file("extdata", "tree.nex", 
-                                   package = "phyloraster"))
-data <- phylo.pres(ras, tree, pruning = "tree")
-
-t <- rast.pd.ses(data$x, edge.path = data$edge.path, 
-                 branch.length = data$branch.length, aleats = 10, 
-                 random = "spat")
-
-## ----pds-plot, fig.height = 5, fig.width = 7, fig.align = 'center', warning= FALSE----
-plot(t)
 
 ## ----sr-plot------------------------------------------------------------------
 # load the data
@@ -186,4 +190,35 @@ terra::plot(riq.fut)
 ## ----dg-----------------------------------------------------------------------
 dg <- delta.grid(riq.pres, riq.fut)
 plot(dg)
+
+## -----------------------------------------------------------------------------
+library(SESraster)
+ras <- terra::rast(system.file("extdata", "rast.presab.tif", 
+                               package = "phyloraster"))
+tree <- ape::read.tree(system.file("extdata", "tree.nex", 
+                                   package = "phyloraster"))
+dataprep <- phylo.pres(ras, tree, full_tree_metr = TRUE)
+
+t <- rast.pd.ses(dataprep$x, edge.path = dataprep$edge.path, 
+                 branch.length = dataprep$branch.length, aleats = 10)
+
+## ----pds-plot, fig.height = 5, fig.width = 7, fig.align = 'center', warning= FALSE----
+plot(t)
+
+## -----------------------------------------------------------------------------
+ras <- terra::rast(system.file("extdata", "rast.presab.tif",
+                               package="phyloraster"))
+tree <- ape::read.tree(system.file("extdata", "tree.nex",
+                                   package="phyloraster"))
+data <- phylo.pres(ras, tree, full_tree_metr = TRUE)
+
+## -----------------------------------------------------------------------------
+ses <- rast.pe.ses(x = data$x, tree = data$tree, aleats = 30, metric = "all")
+names(ses)
+
+## -----------------------------------------------------------------------------
+# CANAPE
+canape <- canape.rast(ses$p.upper.PE, ses$p.upper.PE.alt,
+ses$p.upper.RPE, ses$p.lower.RPE)
+terra::plot(canape)
 
